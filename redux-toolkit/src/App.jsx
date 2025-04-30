cf92a6b633323dae5c673a450094f6a12b0b8db6
Sudeep <sudeepks1005@gmail.com>
2025-04-30 23:45:45 +0530

    redux-tolkit

---
+++ redux-toolkit/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <h2 style={{textAlign:"center", backgroundColor:"black",color:"white", width:"80%",margin:"auto"}}>Helloo Guys</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
