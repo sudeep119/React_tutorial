c96ff5df94502293d7295bb9e27197c6045013ba
Sudeep <sudeepks1005@gmail.com>
2025-06-10 23:20:21 +0530

    update

--- react-practise/src/App.jsx
+++ react-practise/src/App.jsx
@@ -5,29 +5,31 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [word,setWord]=useState('Enter here')
+  const[radio3,setRadio3]=useState(false)
 
+  function onChangeFunc(event){
+      setWord(event.target.value)
+  }
+
+  function functOnselect(e){
+      setRadio3(true)
+
+  }
   return (
     <>
+      <input type='text' onChange={onChangeFunc} value={word} placeholder='Enter any text'/>
+      <h2>{word}</h2>
+      <input id='radio1' type='radio' name='radio'/>
+      <label htmlFor='radio1'>Radio 1</label>
+      <input id='radio2' type='radio' name='radio'/>
+      <label htmlFor='radio2'>Radio 2</label>
+      <input id='radio3' type='radio' name='radio' onChange={functOnselect}/>
+      <label htmlFor='radio3'>Radio 3</label>
+      <h2></h2>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {radio3?<h2>The radio 3 button is selected</h2>:<h2>Not selected</h2>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
