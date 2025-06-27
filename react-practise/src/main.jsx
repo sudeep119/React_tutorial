3704ffa967c234f8585101ca0aec0458255a4a25
Sudeep <sudeepks1005@gmail.com>
2025-06-28 01:42:31 +0530

    upd

--- react-practise/src/main.jsx
+++ react-practise/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import FileSubmit from './components/FileSubmit.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <FileSubmit/>
   </StrictMode>,
 )
